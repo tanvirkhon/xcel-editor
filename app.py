b3fe61228d44c68aefb117c57af0b60488d46aa1
Hoard Coin <119143763+tanvirkhon@users.noreply.github.com>
2023-05-25 15:04:38 -0500

    Starter files

---
+++ app.py
@@ -0,0 +1,72 @@
+import streamlit as st  # pip install streamlit
+import pandas as pd  # pip install pandas
+import plotly.express as px  # pip install plotly-express
+import base64  # Standard Python Module
+from io import StringIO, BytesIO  # Standard Python Module
+
+### Functions ###
+def generate_excel_download_link(df):
+    # Credit Excel: https://discuss.streamlit.io/t/how-to-add-a-download-excel-csv-function-to-a-button/4474/5
+    towrite = BytesIO()
+    df.to_excel(towrite, index=False, header=True)  # write to BytesIO buffer
+    towrite.seek(0)  # reset pointer
+    b64 = base64.b64encode(towrite.read()).decode()
+    href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="data_download.xlsx">Download Excel File</a>'
+    return st.markdown(href, unsafe_allow_html=True)
+
+def generate_html_download_link(fig):
+    # Credit Plotly: https://discuss.streamlit.io/t/download-plotly-plot-as-html/4426/2
+    towrite = StringIO()
+    fig.write_html(towrite, include_plotlyjs="cdn")
+    towrite = BytesIO(towrite.getvalue().encode())
+    b64 = base64.b64encode(towrite.read()).decode()
+    href = f'<a href="data:text/html;charset=utf-8;base64, {b64}" download="plot.html">Download Plot</a>'
+    return st.markdown(href, unsafe_allow_html=True)
+
+### Styles ###
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"] {
+background-color: #000;
+}
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
+</style>
+"""
+
+st.set_page_config(page_title='Pricema Data Planner', page_icon='🤖', layout='wide')
+st.markdown(page_bg_img, unsafe_allow_html=True)
+st.title('Pricema Data Analyser 📈')
+st.subheader('Upload your file')
+
+uploaded_file = st.file_uploader('Choose a XLSX file', type='xlsx')
+if uploaded_file:
+    st.markdown('---')
+    df = pd.read_excel(uploaded_file, engine='openpyxl')
+    st.dataframe(df)
+    groupby_column = st.selectbox(
+        'What would you like to analyse?',
+        ('Ship Mode', 'Segment', 'Category', 'Sub-Category'),
+    )
+
+    # -- GROUP DATAFRAME
+    output_columns = ['Sales', 'Profit']
+    df_grouped = df.groupby(by=[groupby_column], as_index=False)[output_columns].sum()
+
+    # -- PLOT DATAFRAME
+    fig = px.bar(
+        df_grouped,
+        x=groupby_column,
+        y='Sales',
+        color='Profit',
+        color_continuous_scale=['red', 'yellow', 'green'],
+        template='seaborn',
+        title=f'<b>Sales & Profit by {groupby_column}</b>'
+    )
+    st.plotly_chart(fig)
+
+    # -- DOWNLOAD SECTION
+    st.subheader('Downloads:')
+    generate_excel_download_link(df_grouped)
+    generate_html_download_link(fig)
